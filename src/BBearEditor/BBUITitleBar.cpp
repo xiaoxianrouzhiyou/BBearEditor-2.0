7e8f020bc457753beadf24671ed6c66708757605
Bear <184346971@qq.com>
2021-05-09 21:40:39 +0800

    add functions of title bar

--- src/BBearEditor/BBUITitleBar.cpp
+++ src/BBearEditor/BBUITitleBar.cpp
@@ -6,9 +6,45 @@ BBUITitleBar::BBUITitleBar(QWidget *parent) :
     m_pUi(new Ui::BBUITitleBar)
 {
     m_pUi->setupUi(this);
+    this->m_pParent = parent;
+    m_pUi->buttonMaximize->setCheckable(true);
+    m_pUi->buttonMaximize->setChecked(false);
+    m_pUi->buttonRun->setCheckable(true);
+    m_pUi->buttonRun->setChecked(false);
+    QObject::connect(m_pUi->buttonRun, SIGNAL(clicked(bool)), this, SLOT(buttonRunClicked(bool)));
 }
 
 BBUITitleBar::~BBUITitleBar()
 {
     delete m_pUi;
+    delete m_pParent;
+}
+
+void BBUITitleBar::closeWindow()
+{
+    m_pParent->close();
+}
+
+void BBUITitleBar::minimizeWindow()
+{
+    m_pParent->showMinimized();
+}
+
+void BBUITitleBar::maximizeWindow()
+{
+    if (m_pUi->buttonMaximize->isChecked())
+        m_pParent->showFullScreen();
+    else
+        m_pParent->showMaximized();
+}
+
+void BBUITitleBar::buttonRunClicked(bool trigger)
+{
+    run(trigger);
+}
+
+void BBUITitleBar::mouseDoubleClickEvent(QMouseEvent *event)
+{
+    QWidget::mouseDoubleClickEvent(event);
+    m_pParent->move(0, 0);
 }
--- src/BBearEditor/BBUITitleBar.h
+++ src/BBearEditor/BBUITitleBar.h
@@ -2,6 +2,7 @@
 #define BBUITITLEBAR_H
 
 #include <QWidget>
+#include <QMouseEvent>
 
 namespace Ui {
 class BBUITitleBar;
@@ -15,8 +16,19 @@ public:
     explicit BBUITitleBar(QWidget *parent = 0);
     ~BBUITitleBar();
 
+signals:
+    void run(bool trigger);
+
+private slots:
+    void closeWindow();
+    void minimizeWindow();
+    void maximizeWindow();
+    void buttonRunClicked(bool trigger);
+    void mouseDoubleClickEvent(QMouseEvent *event) override;
+
 private:
     Ui::BBUITitleBar *m_pUi;
+    QWidget *m_pParent;
 };
 
 #endif // BBUITITLEBAR_H
