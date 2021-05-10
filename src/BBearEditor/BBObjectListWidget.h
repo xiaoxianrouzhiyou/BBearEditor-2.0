1a3da6d9c8500dd532c4c60750c929ef77362373
Bear <184346971@qq.com>
2021-05-10 21:23:52 +0800

    add object type mark

--- src/BBearEditor/BBObjectListWidget.h
+++ src/BBearEditor/BBObjectListWidget.h
@@ -11,9 +11,12 @@ public:
     explicit BBObjectListWidget(QWidget *parent = 0, const int pieceSize = 35);
     ~BBObjectListWidget();
     bool loadListItems(const char *xmlFilePath);
+    void setObjectType(QString strObjectType) { m_strObjectType = strObjectType; }
 
 private:
     int m_iPieceSize;
+    // {Base, Light, ...}
+    QString m_strObjectType;
 };
 
 #endif // BBOBJECTLISTWIDGET_H
