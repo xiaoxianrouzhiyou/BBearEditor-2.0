6a0510f304127ed7504738cb94adf8b19ab53813
Bear <184346971@qq.com>
2021-07-11 21:49:10 +0800

    improve BBCoordinateSystem2D

--- Code/BBearEditor/Engine/CoordinateSystem/BBCoordinateSystem2D.cpp
+++ Code/BBearEditor/Engine/CoordinateSystem/BBCoordinateSystem2D.cpp
@@ -1,5 +1,6 @@
 #include "BBCoordinateSystem2D.h"
 #include "BBCoordinateComponent2D.h"
+#include "Geometry/BBBoundingBox.h"
 
 
 /**
@@ -8,12 +9,76 @@
 BBCoordinateSystem2D::BBCoordinateSystem2D()
     : BBGameObject()
 {
+    m_pBoundingBoxX = new BBRectBoundingBox2D(0.75f, 0.0f, 0.0f, 0.45f, 0.1f, 0.0f);
+    m_pBoundingBoxY = new BBRectBoundingBox2D(0.0f, 0.75f, 0.0f, 0.1f, 0.45f, 0.0f);
+    m_pBoundingBoxXOY = new BBRectBoundingBox2D(0.15f, 0.15f, 0.0f, 0.15f, 0.15f, 0.0f);
+}
+
+BBCoordinateSystem2D::~BBCoordinateSystem2D()
+{
+    BB_SAFE_DELETE(m_pBoundingBoxX);
+    BB_SAFE_DELETE(m_pBoundingBoxY);
+    BB_SAFE_DELETE(m_pBoundingBoxXOY);
+}
+
+void BBCoordinateSystem2D::setPosition(const QVector3D &position, bool bUpdateLocalTransform)
+{
+    m_pBoundingBoxX->setPosition(position, bUpdateLocalTransform);
+    m_pBoundingBoxY->setPosition(position, bUpdateLocalTransform);
+    m_pBoundingBoxXOY->setPosition(position, bUpdateLocalTransform);
+}
+
+void BBCoordinateSystem2D::setScale(float scale, bool bUpdateLocalTransform)
+{
+    m_pBoundingBoxX->setScale(scale, bUpdateLocalTransform);
+    m_pBoundingBoxY->setScale(scale, bUpdateLocalTransform);
+    m_pBoundingBoxXOY->setScale(scale, bUpdateLocalTransform);
+}
+
+bool BBCoordinateSystem2D::hitAxis(int x, int y, float &fDistance)
+{
+
+}
+
+bool BBCoordinateSystem2D::hitFace(int x, int y, float &fDistance)
+{
 
 }
 
 void BBCoordinateSystem2D::setSelectedObject(BBGameObject *pObject)
 {
 
+}
+
+bool BBCoordinateSystem2D::mouseMoveEvent(int x, int y, bool bMousePressed)
+{
+    do {
+        // if transforming, there is no need to perform other operations
+        BB_END(m_bTransforming);
+        // otherwise, determine whether transform can be turned on
+
+        BB_END(m_pSelectedObject == NULL);
+
+        // handle collision detection, change color of related axis and get m_SelectedAxis
+        // if hitting m_pCoordinateRectFace, no need to handle axis
+        float fDistance;
+        if (!hitFace(x, y, fDistance))
+        {
+            // handle axis
+            BB_END(!hitAxis(x, y, fDistance));
+        }
+
+        // do not handle transform when mouse is not pressed
+        BB_END(!bMousePressed);
+
+        // meet the conditions, turn on m_bTransforming
+        m_bTransforming = true;
+    } while(0);
+
+    transform(x, y);
+
+    // The return value indicates whether the transform has really performed
+    return m_bTransforming;
 }
 
 void BBCoordinateSystem2D::stopTransform()
@@ -56,12 +121,7 @@ void BBPositionCoordinateSystem2D::setSelectedAxis(const BBAxisFlags &axis)
 
 }
 
-bool BBPositionCoordinateSystem2D::mouseMoveEvent(const BBRay &ray, bool bMousePressed)
-{
-
-}
-
-void BBPositionCoordinateSystem2D::transform(const BBRay &ray)
+void BBPositionCoordinateSystem2D::transform(int x, int y)
 {
 
 }
--- Code/BBearEditor/Engine/CoordinateSystem/BBCoordinateSystem2D.h
+++ Code/BBearEditor/Engine/CoordinateSystem/BBCoordinateSystem2D.h
@@ -5,23 +5,34 @@
 #include "Base/BBGameObject.h"
 
 class BBPositionCoordinateComponent2D;
+class BBRectBoundingBox2D;
 
 class BBCoordinateSystem2D : public BBGameObject
 {
 protected:
     BBCoordinateSystem2D();
+    ~BBCoordinateSystem2D();
 
-    virtual void transform(const BBRay &ray) = 0;
+    void setPosition(const QVector3D &position, bool bUpdateLocalTransform = true) override;
+    void setScale(float scale, bool bUpdateLocalTransform = true) override;
+
+    virtual bool hitAxis(int x, int y, float &fDistance);
+    virtual bool hitFace(int x, int y, float &fDistance);
+    virtual void transform(int x, int y) = 0;
 
     BBAxisFlags m_SelectedAxis;
     QVector3D m_LastMousePos;
     BBGameObject *m_pSelectedObject;
     bool m_bTransforming;
 
+    BBRectBoundingBox2D *m_pBoundingBoxX;
+    BBRectBoundingBox2D *m_pBoundingBoxY;
+    BBRectBoundingBox2D *m_pBoundingBoxXOY;
+
 public:
     void setSelectedObject(BBGameObject *pObject);
     virtual void setSelectedAxis(const BBAxisFlags &axis) = 0;
-    virtual bool mouseMoveEvent(const BBRay &ray, bool bMousePressed) = 0;
+    bool mouseMoveEvent(int x, int y, bool bMousePressed);
     void stopTransform();
 };
 
@@ -37,10 +48,9 @@ public:
     void resize(float fWidth, float fHeight) override;
 
     void setSelectedAxis(const BBAxisFlags &axis) override;
-    bool mouseMoveEvent(const BBRay &ray, bool bMousePressed) override;
 
 private:
-    void transform(const BBRay &ray) override;
+    void transform(int x, int y) override;
 
     BBPositionCoordinateComponent2D *m_pCoordinateComponent;
 };
